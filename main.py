ad297e1247b5a6c02a36a1c8e9e3365162377c6d
Rx-crxsh3r <ahmed.ab2824@gmail.com>
2024-07-28 23:59:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import socket
+
+ip1 = 0
+
+def get_ip():
+    global ip1
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    ip1 = IP
+
+get_ip()
+print(ip1)
